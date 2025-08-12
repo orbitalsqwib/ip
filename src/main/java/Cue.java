6c5f2addca4d1ee4d50ce5e1c950d7ca9cc19f18
Eugene L <orbital@sqwib.works>
2025-08-13 03:01:28 +0800

    Rename assistant from Duke to Cue
    Duke is the template name, which is quite unoriginal.
    
    Rename the assistant to Cue to give it some personality.
    
    - Changed all copy mentions of Duke to Cue
    - Updated fancy assistant greeting to Cue
    - Updated tests for greeting

---
+++ src/main/java/Cue.java
@@ -0,0 +1,10 @@
+public class Cue {
+    public static void main(String[] args) {
+        String logo = "  ▄▄▄x  ▄▄▄ x▄▄    ▄▄▄o  \n"
+                + "o█   ▀▀  ██  ██  ▄█▄▄x██ \n"
+                + "██       ██  ██  ██      \n"
+                + " ▀█x▄▄▀  ▀█o▄▀█x  ▀█x▄▄▀ \n";
+
+        System.out.println("Hello from\n" + logo);
+    }
+}
