668a1a1586b459c277dd81798c36432652ca338b
Eugene L <orbital@sqwib.works>
2025-08-13 16:13:54 +0800

    Task: Fix indent to use 4 spaces

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -1,25 +1,25 @@
 package task;
 
 public class Task {
-  private boolean done;
-  private String taskName;
+    private boolean done;
+    private String taskName;
 
-  public Task(String taskName) {
-    this.taskName = taskName;
-    this.done = false;
-  }
-
-  public void setDone(boolean isDone) {
-    this.done = isDone;
-  }
-
-  @Override
-  public String toString() {
-    char status = ' ';
-    if (this.done) {
-      status = 'x';
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.done = false;
     }
 
-    return "[" + status + "] " + this.taskName;
-  }
+    public void setDone(boolean isDone) {
+        this.done = isDone;
+    }
+
+    @Override
+    public String toString() {
+        char status = ' ';
+        if (this.done) {
+            status = 'x';
+        }
+
+        return "[" + status + "] " + this.taskName;
+    }
 }
